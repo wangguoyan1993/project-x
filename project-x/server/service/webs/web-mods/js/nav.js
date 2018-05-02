68dccad82b37aa44cdeffb28ac298383d6ec562e
Macanglian <774337748@qq.com>
2018-05-02 16:54:30 +0800

    修改主页面左侧导航栏

--- project-x/server/service/webs/web-mods/js/nav.js
+++ project-x/server/service/webs/web-mods/js/nav.js
@@ -1,45 +1,61 @@
 var navs = [{
-	"title" : "后台首页",
+	"title" : "实验室管理主要功能",
 	"icon" : "icon-computer",
 	"href" : "page/main.html",
 	"spread" : false
 },{
-	"title" : "文章列表",
+	"title" : "实验室预约",
 	"icon" : "icon-text",
 	"href" : "page/news/newsList.html",
 	"spread" : false
 },{
-	"title" : "友情链接",
+	"title" : "实验课程浏览",
 	"icon" : "icon-text",
 	"href" : "page/links/linksList.html",
 	"spread" : false
 },{
-	"title" : "404页面",
+	"title" : "实验教材教学",
 	"icon" : "&#xe61c;",
 	"href" : "page/404.html",
 	"spread" : false
 },{
-	"title" : "系统基本参数",
+	"title" : "实验室设备",
 	"icon" : "&#xe631;",
 	"href" : "page/systemParameter/systemParameter.html",
 	"spread" : false
 },{
-	"title" : "二级菜单演示",
+	"title" : "虚拟仿真教学",
 	"icon" : "&#xe61c;",
 	"href" : "",
 	"spread" : false,
 	"children" : [
 		{
-			"title" : "二级菜单1",
+			"title" : "物理实验",
 			"icon" : "&#xe631;",
 			"href" : "",
 			"spread" : false
 		},
 		{
-			"title" : "二级菜单2",
+			"title" : "化学实验",
 			"icon" : "&#xe631;",
 			"href" : "",
 			"spread" : false
-		}
+		},
+        {
+            "title" : "生物实验",
+            "icon" : "&#xe631;",
+            "href" : "",
+            "spread" : false
+        }
 	]
+},{
+    "title" : "实验信息交流区",
+    "icon" : "&#xe631;",
+    "href" : "page/systemParameter/systemParameter.html",
+    "spread" : false
+},{
+    "title" : "实验室监控",
+    "icon" : "&#xe631;",
+    "href" : "page/systemParameter/systemParameter.html",
+    "spread" : false
 }]
