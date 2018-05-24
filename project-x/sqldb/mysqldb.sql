b3c3f8af297ffebedb9e1dc6ff84d88fde8db320
wangguoyan1993 <2282940593@qq.com>
2018-05-24 23:51:17 +0800

    - 课程和合成类型建表

--- project-x/sqldb/mysqldb.sql
+++ project-x/sqldb/mysqldb.sql
@@ -27,11 +27,47 @@ CREATE TABLE `main` (
   `number` int(64) NOT NULL COMMENT '用户账号',
   `password` varchar(64) NOT NULL COMMENT '用户密码',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `main` */
 
-insert  into `main`(`id`,`name`,`type`,`number`,`password`) values (1,'张三',1,12405008,'123'),(2,'李四',1,16405008,'qwe123'),(3,'王五',2,11407001,'111222333'),(4,'汪国岩',3,19930525,'19930525'),(5,'王彦春',2,123111,'123');
+insert  into `main`(`id`,`name`,`type`,`number`,`password`) values (1,'张三',1,12405008,'123'),(2,'李四',1,16405008,'qwe123'),(3,'王五',2,11407001,'111222333'),(4,'汪国岩',3,19930525,'19930525'),(5,'王彦春',2,123111,'123'),(6,'李云',2,123123,'111'),(7,'王建',2,111222,'1231'),(8,'吴松',2,1233112,'11122');
+
+/*Table structure for table `t_lesson` */
+
+DROP TABLE IF EXISTS `t_lesson`;
+
+CREATE TABLE `t_lesson` (
+  `id` int(16) NOT NULL AUTO_INCREMENT COMMENT '课程id',
+  `code` varchar(32) NOT NULL COMMENT '课程代码',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  `college` varchar(64) DEFAULT NULL COMMENT '学院',
+  `place` varchar(64) DEFAULT NULL COMMENT '地点',
+  `time` varchar(32) DEFAULT NULL COMMENT '时间',
+  `property` int(8) NOT NULL COMMENT '性质id',
+  `teacher` int(16) NOT NULL COMMENT '用户id',
+  `credit` int(8) DEFAULT NULL COMMENT '学分',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_lesson` */
+
+insert  into `t_lesson`(`id`,`code`,`name`,`college`,`place`,`time`,`property`,`teacher`,`credit`) values (1,'B1400107','物理实验A1','物理科学与技术学院','崇理楼410','周三下午7-8节',1,6,3);
+
+/*Table structure for table `t_lesson_class` */
+
+DROP TABLE IF EXISTS `t_lesson_class`;
+
+CREATE TABLE `t_lesson_class` (
+  `id` int(8) NOT NULL AUTO_INCREMENT COMMENT '课程类型id',
+  `name` varchar(32) NOT NULL COMMENT '课程类型名称',
+  `code` varchar(16) NOT NULL COMMENT '课程类型代码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_lesson_class` */
+
+insert  into `t_lesson_class`(`id`,`name`,`code`) values (1,'必修课','1001'),(2,'专业选修课','1002'),(3,'公共选修课','1003');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
