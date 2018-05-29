09fe4643d512747173dc113a47d0873cdf2e3c73
wangguoyan1993 <2282940593@qq.com>
2018-05-29 12:30:57 +0800

    - 数据表添加数据

--- project-x/sqldb/mysqldb.sql
+++ project-x/sqldb/mysqldb.sql
@@ -29,6 +29,10 @@ CREATE TABLE `main` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
+/*Data for the table `main` */
+
+insert  into `main`(`id`,`name`,`type`,`number`,`user_password`) values (1,'张三',1,12405008,'123'),(2,'李四',1,16405008,'qwe123'),(3,'王五',2,11407001,'111222333'),(4,'汪国岩',3,19930525,'19930525'),(5,'王彦春',2,123111,'111111'),(6,'李云',2,123123,'111'),(7,'王建',2,111222,'1231'),(8,'吴松',2,1233112,'11122');
+
 /*Table structure for table `t_lab` */
 
 DROP TABLE IF EXISTS `t_lab`;
@@ -42,6 +46,10 @@ CREATE TABLE `t_lab` (
   `time` varchar(128) DEFAULT NULL COMMENT '上课时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_lab` */
+
+insert  into `t_lab`(`id`,`name`,`code`,`collage`,`place`,`time`) values (1,'惠斯通电桥实验室','B1400401','物理科学与技术学院','崇理楼401','周二，1-3节；周三，5-7节'),(2,'材料化学实验室','B1400322','环境工程与化学学院','化明楼322','周三，1-3节；周五，1-3节'),(3,'微生物实验室','B1400208','生命科学与技术学院','生命学院208','周三，1-3节；周五，1-3节');
 
 /*Table structure for table `t_lab_order` */
 
@@ -62,6 +70,10 @@ CREATE TABLE `t_lab_order` (
   CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `main` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*Data for the table `t_lab_order` */
+
+insert  into `t_lab_order`(`lab_id`,`uid`,`start_time`,`end_time`,`status_id`,`reason`) values (1,6,'1111','12112',2,'6');
+
 /*Table structure for table `t_lab_order_status` */
 
 DROP TABLE IF EXISTS `t_lab_order_status`;
@@ -71,6 +83,10 @@ CREATE TABLE `t_lab_order_status` (
   `name` varchar(16) NOT NULL COMMENT '预约状态描述',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_lab_order_status` */
+
+insert  into `t_lab_order_status`(`id`,`name`) values (1,'申请'),(2,'成功'),(3,'失败');
 
 /*Table structure for table `t_lesson` */
 
@@ -93,6 +109,10 @@ CREATE TABLE `t_lesson` (
   CONSTRAINT `teacher` FOREIGN KEY (`teacher`) REFERENCES `main` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+/*Data for the table `t_lesson` */
+
+insert  into `t_lesson`(`id`,`code`,`name`,`college`,`place`,`time`,`property`,`teacher`,`credit`) values (1,'B1400107','物理实验A1','物理科学与技术学院','崇理楼410','周三下午7-8节',1,6,3),(2,'B1400203','化学实验A1','环境工程与化学学院','环化楼320','周一7-8节；周四3-4节',2,7,3),(3,'B1400019','普通生物学实验A','生命科学与技术','生命学院204','周二5-6节',1,8,3);
+
 /*Table structure for table `t_lesson_class` */
 
 DROP TABLE IF EXISTS `t_lesson_class`;
@@ -103,6 +123,10 @@ CREATE TABLE `t_lesson_class` (
   `code` varchar(16) NOT NULL COMMENT '课程类型代码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_lesson_class` */
+
+insert  into `t_lesson_class`(`id`,`name`,`code`) values (1,'必修课','1001'),(2,'专业选修课','1002'),(3,'公共选修课','1003');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
